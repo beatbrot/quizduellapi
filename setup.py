0cd32750c14ade0f0a28a13127e93de6c93121f3
beatbrot <loy.christoph@gmail.com>
2018-05-11 13:52:28 +0200

    Add setup.py for usage with pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='quizduell-api',
+      version='1.0',
+      description='Inofficial interface to the Quizduell web API written in Python and distributed under GPLv3',
+      author='mtschirs, beatbrot',
+      url='https://github.com/beatbrot/quizduellapi',
+      packages=['quizduell'],
+      license='GPLv3')
